a1b075fdb08a00f6b7a6d8af56119066e262675c
Sergey Starikov <sergey.starikov@apm-consult.com>
2018-09-07 14:52:36 +0300

    Services added

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+	Owner findByFirstName(String firstName);
+
+	Owner findById(Long id);
+
+	Owner save(Owner owner);
+
+	Set<Owner> findAll();
+}
