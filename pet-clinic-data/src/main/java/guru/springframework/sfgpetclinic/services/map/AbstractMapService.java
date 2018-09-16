899c6b4b0cd43c7f29e44721aba1173ebf213464
Sergey Starikov <sergey.starikov@apm-consult.com>
2018-09-16 21:30:13 +0300

    Auto generation for map IDs added

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,13 +1,16 @@
 package guru.springframework.sfgpetclinic.services.map;
 
+import guru.springframework.sfgpetclinic.model.BaseEntity;
+
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, ID> {
+public abstract class AbstractMapService<T extends BaseEntity, ID> {
 
-	protected Map<ID, T> map = new HashMap<>();
+	protected Map<Long, T> map = new HashMap<>();
 
 	Set<T> findAll() {
 		return new HashSet<>(map.values());
@@ -17,8 +20,16 @@ public abstract class AbstractMapService<T, ID> {
 		return map.get(id);
 	}
 
-	T save(ID id, T object) {
-		map.put(id, object);
+	T save(T object) {
+
+		if (object != null) {
+			if (object.getId() == null) {
+				object.setId(getNextId());
+			}
+		} else {
+			throw new RuntimeException("Object cannot be null");
+		}
+		map.put(object.getId(), object);
 
 		return object;
 	}
@@ -29,6 +40,13 @@ public abstract class AbstractMapService<T, ID> {
 
 	void delete(T object) {
 		map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+	}
+
+	private Long getNextId() {
+		if (map.isEmpty()) {
+			return 1L;
+		}
+		return Collections.max(map.keySet()) + 1;
 	}
 
 }
