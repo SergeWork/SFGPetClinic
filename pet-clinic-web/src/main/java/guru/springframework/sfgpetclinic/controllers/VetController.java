3aa2e9f45fdd7258b8eaa0fc82dff21fdb2f62f2
Sergey Starikov <sergey.starikov@apm-consult.com>
2018-09-16 21:19:50 +0300

    Owners and vets lists added

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,15 +1,28 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
 
 	public static final String VETS_INDEX = "vets/index";
 
+	private final VetService vetService;
+
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
 	@RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-	public String listVets() {
+	public String listVets(Model model) {
+		Set<Vet> vets = vetService.findAll();
+		model.addAttribute("vets", vets);
 		return VETS_INDEX;
 	}
 }
