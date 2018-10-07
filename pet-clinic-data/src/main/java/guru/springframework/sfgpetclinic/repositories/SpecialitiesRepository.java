7a8799cc3f32ac9e8024e6b8bca30eff2132fa97
Sergey Starikov <sergey.starikov@apm-consult.com>
2018-10-07 10:55:52 +0300

    Repositories added

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/SpecialitiesRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpecialitiesRepository extends CrudRepository<Speciality, Long> {
+
+}
